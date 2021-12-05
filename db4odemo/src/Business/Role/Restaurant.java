d363521e1fe0a48d3e5323fcf810bb4fe38f069c
Netra Awate <awate.n@northeastern.edu>
2021-12-05 00:14:57 -0500

    create UI workareas for Restaurant and Accessories java class

--- db4odemo/src/Business/Role/Restaurant.java
+++ db4odemo/src/Business/Role/Restaurant.java
@@ -10,6 +10,7 @@ import Business.Enterprise.Enterprise;
 import Business.Organization.Organization;
 import Business.UserAccount.UserAccount;
 import javax.swing.JPanel;
+import userinterface.Restaurant.RestaurantWorkAreaJPanel;
 
 /**
  *
